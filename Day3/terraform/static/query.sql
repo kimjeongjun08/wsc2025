4dd9ed6eec35cadce8557a88aa1c9989a7584609
kimjeongjun08 <iamkimjj0307@gmail.com>
2025-09-23 00:10:18 +0900

    Update query.sql

--- Day3/terraform/static/query.sql
+++ Day3/terraform/static/query.sql
@@ -1,8 +1,1 @@
-use dev;
-CREATE TABLE user (
-    userid INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL,
-    status_message VARCHAR(255)
-);
 CREATE INDEX idx_email ON user(email);
